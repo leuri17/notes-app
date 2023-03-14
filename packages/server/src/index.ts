68307477da3c20c3c40eec77c6ab139130ed439a
ALONSO SATURRIA Leuri Yunior <leuri-yunior.alonso@soprasteria.com>
2023-03-14 11:52:03 +0100

    DB connection done

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,35 @@
-import express from 'express';
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+// import { userService } from './services/UserService.js'
+// import { noteService } from './services/NoteService.js'
 
-const app = express();
+// Initializations
+const app: Express = express()
+const { ENV, DB_PROD_URI, DB_DEV_URI } = process.env
+const connectionString = ENV === 'dev' ? DB_DEV_URI : DB_PROD_URI
 
-// declare a route with a response
+// DB Connection
+mongoose
+  .connect(connectionString as string)
+  .then((db) => {
+    console.log('Connected to DB:', db.connection.name)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
+// Settings
+app.set('port', process.env.PORT || 3000)
+
+// Middlewares
+
+// Routes
 app.get('/', (req, res) => {
-  res.send("Hello Woooaaarld!");
-});
+  res.send('<h1>Hello World!</h1>')
+})
 
-// start the server
-app.listen(8081, () => {
-  console.log(`server running : http://localhost:8081`);
-});
+// Server
+app.listen(app.get('port'), () => {
+  console.log(`🚀 Server listening on port ${app.get('port')}`)
+})
